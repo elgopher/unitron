9388f2f24fb5a373158adb9a76ca18ba200b94d8
Jacek Olszak <jacekolszak@gmail.com>
2024-11-08 00:34:38 +0100

    Typo in succeeded word

--- gui/test_summary.lua
+++ gui/test_summary.lua
@@ -2,18 +2,18 @@
 -- This code is licensed under MIT license (see LICENSE for details)
 
 function attach_test_summary(gui, el)
-    local succeded, failed = 0, 0
+    local succeeded, failed = 0, 0
 
     local container = gui:attach(el)
 
     function container:draw()
         rectfill(0, 0, el.width, el.height, 0)
         color(26)
-        print("Succeded: " .. succeded .. " \f8 Failed: " .. failed)
+        print("Succeeded: " .. succeeded .. " \f8 Failed: " .. failed)
     end
 
-    function container:inc_succeded()
-        succeded += 1
+    function container:inc_succeeded()
+        succeeded += 1
     end
 
     function container:inc_failed()
