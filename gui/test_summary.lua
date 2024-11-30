99160801de943c57134edebefa110965b3a8c85c
Jacek Olszak <jacekolszak@gmail.com>
2024-11-30 14:48:40 +0100

    [bugfix] Sometimes links dont work in test output
    Because there are overlapping (yet hidden) components - test summary and
    "lights".

--- gui/test_summary.lua
+++ gui/test_summary.lua
@@ -6,14 +6,20 @@ function attach_test_summary(parent, el)
 	local succeeded, failed = 0, 0
 
 	el = parent:attach(el)
-	el.visible = true -- Picotron's hidden field is broken
+	el.height_before_hide = el.height
+
+	-- Picotron's hidden field is broken
+	function el:set_visible(v)
+		if not v then
+			el.height = 0
+		else
+			el.height = el.height_before_hide
+		end
+	end
 
 	function el:draw()
-		if not el.visible then
-			return
-		end
-
 		rectfill(0, 0, el.width, el.height, 0)
+
 		color(26)
 		print("Succeeded: " .. succeeded .. " \f8 Failed: " .. failed)
 	end
